3a79d4bd722c27611ab5554fbb5bee848895fa58
Gabriel Mendes <gfmendes@gmail.com>
2021-10-09 17:28:24 -0300

    criacao de constantes

---
+++ src/main/java/br/com/sistemasvendas/util/Constants.java
@@ -0,0 +1,21 @@
+package br.com.sistemasvendas.util;
+
+public class Constants {
+
+	public static final String ACTION_KEY = "action";
+	public static final String NEW_ACTION = "new";
+	public static final String INSERT_ACTION = "insert";
+	public static final String DELETE_ACTION = "delete";
+	public static final String EDIT_ACTION = "edit";
+	public static final String UPDATE_ACTION = "update";
+
+	public static final String ID_COL_NAME = "id";
+	public static final String NAME_COL_NAME = "name";
+	public static final String CPF_COL_NAME = "cpf";
+	public static final String CNPJ_COL_NAME = "cnpj";
+
+	public static final String USER_ID_COL_NAME = "user_id";
+	public static final String DESCRICAO_COL_NAME = "descricao";
+	public static final String PRECO_COL_NAME = "preco";
+
+}
