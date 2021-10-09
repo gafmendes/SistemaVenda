81fdbe2edfd012b071f8af7b4ac16e030090814c
Gabriel Mendes <gfmendes@gmail.com>
2021-10-09 17:12:03 -0300

    configuracao de conexao

--- src/main/java/br/com/sistemasvendas/util/HibernateUtil.java
+++ src/main/java/br/com/sistemasvendas/util/HibernateUtil.java
@@ -1,6 +1,15 @@
 package br.com.sistemasvendas.util;
 
+import java.util.Properties;
+
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+
+import br.com.sistemasvendas.model.Product;
+import br.com.sistemasvendas.model.User;
 
 public class HibernateUtil {
 
@@ -9,6 +18,28 @@ public class HibernateUtil {
 	public static SessionFactory getSessionFactory() {
 		if (sessionFactory == null) {
 			try {
+				Configuration configuration = new Configuration();
+				Properties settings = new Properties();
+				settings.put(Environment.DRIVER, "com.mysql.jdbc.Driver");
+				settings.put(Environment.URL, "jdbc:mysql://localhost:3306/SistemaVendas?createDatabaseIfNotExist=true&useSSL=false&useTimezone=true&serverTimezone=UTC");
+				settings.put(Environment.USER, "root");
+				settings.put(Environment.PASS, "root");
+				
+				settings.put(Environment.SHOW_SQL, "true");
+				
+				settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+				
+				settings.put(Environment.HBM2DDL_AUTO, "update");
+				
+				configuration.setProperties(settings);
+				configuration.addAnnotatedClass(User.class);
+				configuration.addAnnotatedClass(Product.class);
+				
+				ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+						.applySettings(configuration.getProperties()).build();
+				System.out.println("Configuracao hibernate realizada com sucesso");
+				sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+				return sessionFactory;
 				
 			} catch (Exception e) {
 				e.printStackTrace();
