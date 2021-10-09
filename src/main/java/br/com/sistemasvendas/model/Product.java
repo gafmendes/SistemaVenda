ce416808093ef83cba5aeb8b94875b4a9f510adb
Gabriel Mendes <gfmendes@gmail.com>
2021-10-09 10:07:32 -0300

    criacao do model

--- src/main/java/br/com/sistemasvendas/model/Product.java
+++ src/main/java/br/com/sistemasvendas/model/Product.java
@@ -1,5 +1,60 @@
 package br.com.sistemasvendas.model;
 
+import javax.persistence.Entity;
+
+@Entity
 public class Product {
+
+	private int id;
+
+	private String descricao;
+
+	private Double preco;
+
+	private User user;
+
+	public Product() {
+		super();
+	}
+
+	public Product(int id, String descricao, Double preco, User user) {
+		super();
+		this.id = id;
+		this.descricao = descricao;
+		this.preco = preco;
+		this.user = user;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
 
 }
--- src/main/java/br/com/sistemasvendas/model/User.java
+++ src/main/java/br/com/sistemasvendas/model/User.java
@@ -1,5 +1,73 @@
 package br.com.sistemasvendas.model;
 
+import java.util.List;
+
+import javax.persistence.Entity;
+
+@Entity
 public class User {
+
+	private int id;
+
+	private String name;
+
+	private String cpf;
+
+	private String cnpj;
+
+	private List<Product> product;
+
+	public User() {
+
+	}
+
+	public User(int id, String name, String cpf, String cnpj, List<Product> product) {
+
+		this.id = id;
+		this.name = name;
+		this.cpf = cpf;
+		this.cnpj = cnpj;
+		this.product = product;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getCnpj() {
+		return cnpj;
+	}
+
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+
+	public List<Product> getProduct() {
+		return product;
+	}
+
+	public void setProduct(List<Product> product) {
+		this.product = product;
+	}
 
 }
